66ad127505d47099435e2fa908a14bc60dbe5109
mightbegood12 <banunaresh1976@gmail.com>
2024-12-13 23:05:55 +0530

    [FIX] Responsiveness

--- portfolio/src/app/components/Navbar.jsx
+++ portfolio/src/app/components/Navbar.jsx
@@ -42,7 +42,7 @@ const Navbar = () => {
 
   return (
     <div className="mt-5 absolute right-0 left-0">
-      <div className="bg-[#721e1e]/30 mix-blend-exclusion max-h-[48px] w-max mx-auto transition-all duration-600 text-xs origin-center font-mono font-bold rounded-r-full rounded-l-full gap-2 p-4 flex flex-row items-center justify-center">
+      <div className="bg-[#721e1e]/30 mix-blend-exclusion max-h-[48px] w-max mx-auto transition-all duration-600 text-[10px] md:text-xs origin-center font-mono font-bold rounded-r-full rounded-l-full md:gap-2 p-2 md:p-4 flex flex-row items-center justify-center">
         {tabs.map((tab) => (
           <a
             key={tab.id}
@@ -51,7 +51,7 @@ const Navbar = () => {
               activeTab === tab.id
                 ? "font-bold text-opacity-100"
                 : "text-opacity-60"
-            } cursor-pointer hover:text-sm transition-all duration-300 relative rounded-full px-3 py-1.5 text-white focus-visible:outline outline-red-400`}
+            } cursor-pointer md:hover:text-sm transition-all duration-300 relative rounded-full px-3 py-1.5 text-white focus-visible:outline outline-red-400`}
           >
             {tab.label}
             {activeTab === tab.id && (
@@ -59,7 +59,11 @@ const Navbar = () => {
                 layoutId="bubble"
                 className="absolute inset-0 z-10 bg-white mix-blend-difference"
                 style={{ borderRadius: 9999 }}
-                transition={{ type: "spring", duration: 0.6 }}
+                transition={{
+                  x: { type: "spring", stiffness: 100 },
+                  duration: 0.6,
+                  delay: 0.2,
+                }}
               />
             )}
           </a>
