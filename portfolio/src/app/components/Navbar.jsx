207cbb732ea5591dbaa9b84cf92f4c4362d314b5
mightbegood12 <banunaresh1976@gmail.com>
2024-12-13 13:01:06 +0530

    [ADD] Hero Section

--- portfolio/src/app/components/Navbar.jsx
+++ portfolio/src/app/components/Navbar.jsx
@@ -1,33 +1,69 @@
-import React from "react";
+"use client";
+import { useEffect, useState } from "react";
+import { motion } from "framer-motion";
+
+const tabs = [
+  { id: "hero", label: "HOME" },
+  { id: "about", label: "ABOUT" },
+  { id: "projects", label: "PROJECTS" },
+  { id: "contact", label: "CONTACT" },
+];
 
 const Navbar = () => {
+  const [activeTab, setActiveTab] = useState(tabs[0].id);
+
+  useEffect(() => {
+    const sections = document.querySelectorAll("section");
+    const observerOptions = {
+      root: null,
+      rootMargin: "0px",
+      threshold: 0.6, // More flexible threshold for detecting active section
+    };
+
+    const observerCallback = (entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          setActiveTab(entry.target.id);
+        }
+      });
+    };
+
+    const observer = new IntersectionObserver(
+      observerCallback,
+      observerOptions
+    );
+
+    sections.forEach((section) => observer.observe(section));
+
+    return () => {
+      sections.forEach((section) => observer.unobserve(section));
+    };
+  }, []);
+
   return (
     <div className="mt-5 absolute right-0 left-0">
-      <div className="bg-[#721e1e]/30 w-max mx-auto flex-shrink transition-all duration-300 text-xs origin-center font-mono text-[#ff0000] text-opacity-55 font-bold rounded-r-full rounded-l-full gap-6 p-4 flex flex-row items-center justify-center">
-        <a
-          className="cursor-pointer hover:text-sm hover:text-white/80 transition-all duration-300"
-          href="#hero"
-        >
-          HOME
-        </a>
-        <a
-          className="cursor-pointer hover:text-sm hover:text-white/80 transition-all duration-300"
-          href="#about"
-        >
-          ABOUT
-        </a>
-        <a
-          className="cursor-pointer hover:text-sm hover:text-white/80 transition-all duration-300"
-          href="#projects"
-        >
-          PROJECTS
-        </a>
-        <a
-          className="cursor-pointer hover:text-sm hover:text-white/80 transition-all duration-300"
-          href="#contact"
-        >
-          CONTACT
-        </a>
+      <div className="bg-[#721e1e]/30 mix-blend-exclusion max-h-[48px] w-max mx-auto transition-all duration-600 text-xs origin-center font-mono font-bold rounded-r-full rounded-l-full gap-2 p-4 flex flex-row items-center justify-center">
+        {tabs.map((tab) => (
+          <a
+            key={tab.id}
+            href={`#${tab.id}`}
+            className={`${
+              activeTab === tab.id
+                ? "font-bold text-opacity-100"
+                : "text-opacity-60"
+            } cursor-pointer hover:text-sm transition-all duration-300 relative rounded-full px-3 py-1.5 text-white focus-visible:outline outline-red-400`}
+          >
+            {tab.label}
+            {activeTab === tab.id && (
+              <motion.span
+                layoutId="bubble"
+                className="absolute inset-0 z-10 bg-white mix-blend-difference"
+                style={{ borderRadius: 9999 }}
+                transition={{ type: "spring", duration: 0.6 }}
+              />
+            )}
+          </a>
+        ))}
       </div>
     </div>
   );
--- portfolio/src/app/page.js
+++ portfolio/src/app/page.js
@@ -1,30 +1,45 @@
 import React from "react";
 import { Boxes } from "./components/Boxes";
 import Navbar from "./components/Navbar";
+import { GradualSpacing } from "./components/Gradual_Spacing";
 
 const nicknames = ["DEV", "SON", "GAMER", ""];
 
 const page = () => {
   return (
-    <>
+    <div className="snap-y snap-mandatory">
       <header className="sticky top-0 mx-auto z-50">
         <Navbar />
       </header>
-      <section
-        id="hero"
-        className="h-screen relative w-full overflow-hidden bg-black flex flex-col items-center font-sans rounded-lg"
-      >
-        <div className="absolute inset-0 w-full h-full bg-black z-20 [mask-image:radial-gradient(transparent,white)] pointer-events-none" />
-        <Boxes />
-        <div className="select-none content text-[8rem] mt-20 z-30">
-          <h2>Magesh</h2>
-          <h2>Magesh</h2>
+      <section id="hero" className="h-screen snap-always snap-center w-full">
+        <div className="h-96 w-full overflow-hidden relative flex bg-black font-sans flex-col items-center">
+          <div className="absolute inset-0 w-full h-full bg-black z-20 [mask-image:radial-gradient(transparent,white)] pointer-events-none" />
+          <Boxes />
+          <div className="select-none content text-[8rem] mt-20 z-30">
+            <h2>Magesh</h2>
+            <h2>Magesh</h2>
+          </div>
+          <div className="z-30 select-none text-[4rem] mt-40 ml-[20rem]">
+            <GradualSpacing text={"DEV"} />
+          </div>
         </div>
       </section>
-      <section id="about" className="h-screen bg-black"></section>
-      <section id="projects"></section>
-      <section id="contact"></section>
-    </>
+      <section id="about" className="h-screen snap-always snap-center bg-black">
+        <div className="text-4xl text-center">ABOUT</div>
+      </section>
+      <section
+        id="projects"
+        className="h-screen snap-always snap-center bg-black"
+      >
+        <div className="text-4xl text-center">PROJECTS</div>
+      </section>
+      <section
+        id="contact"
+        className="h-screen snap-always snap-center bg-black"
+      >
+        <div className="text-4xl text-center">CONTACT</div>
+      </section>
+    </div>
   );
 };
 
