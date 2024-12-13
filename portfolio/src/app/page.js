40d3f4ffd45d0e87dbc9815852d39cd6ebae255b
mightbegood12 <banunaresh1976@gmail.com>
2024-12-13 23:05:42 +0530

    [FIX] Responsiveness

--- portfolio/src/app/page.js
+++ portfolio/src/app/page.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Boxes } from "./components/Boxes";
 import Navbar from "./components/Navbar";
 import { GradualSpacing } from "./components/Gradual_Spacing";
+import { TracingBeam } from "./components/TracingBeam";
 
 const nicknames = ["DEV", "SON", "GAMER", ""];
 
@@ -11,34 +12,39 @@ const page = () => {
       <header className="sticky top-0 mx-auto z-50">
         <Navbar />
       </header>
-      <section id="hero" className="h-screen snap-always snap-center w-full">
-        <div className="h-96 w-full overflow-hidden relative flex bg-black font-sans flex-col items-center">
-          <div className="absolute inset-0 w-full h-full bg-black z-20 [mask-image:radial-gradient(transparent,white)] pointer-events-none" />
-          <Boxes />
-          <div className="select-none content text-[8rem] mt-20 z-30">
-            <h2>Magesh</h2>
-            <h2>Magesh</h2>
+      <TracingBeam>
+        <section id="hero" className="h-screen snap-always snap-center w-full">
+          <div className="h-96 w-full overflow-hidden relative flex bg-black font-sans flex-col items-center">
+            <div className="absolute inset-0 w-full h-full bg-black z-20 [mask-image:radial-gradient(transparent,white)] pointer-events-none" />
+            <Boxes />
+            <div className="select-none content text-[4rem] md:text-[8rem] mt-20 z-30">
+              <h2>Magesh</h2>
+              <h2>Magesh</h2>
+            </div>
+            <div className="z-30 select-none text-[4rem] mt-20 ml-[10rem] md:mt-40 md:ml-[20rem]">
+              <GradualSpacing text={"DEV"} />
+            </div>
           </div>
-          <div className="z-30 select-none text-[4rem] mt-40 ml-[20rem]">
-            <GradualSpacing text={"DEV"} />
-          </div>
-        </div>
-      </section>
-      <section id="about" className="h-screen snap-always snap-center bg-black">
-        <div className="text-4xl text-center">ABOUT</div>
-      </section>
-      <section
-        id="projects"
-        className="h-screen snap-always snap-center bg-black"
-      >
-        <div className="text-4xl text-center">PROJECTS</div>
-      </section>
-      <section
-        id="contact"
-        className="h-screen snap-always snap-center bg-black"
-      >
-        <div className="text-4xl text-center">CONTACT</div>
-      </section>
+        </section>
+        <section
+          id="about"
+          className="h-screen snap-always snap-center bg-black flex flex-col justify-center items-center"
+        >
+          <div className="text-4xl text-center ">ABOUT</div>
+        </section>
+        <section
+          id="projects"
+          className="h-screen snap-always snap-center bg-black flex flex-col justify-center items-center"
+        >
+          <div className="text-4xl text-center ">PROJECTS</div>
+        </section>
+        <section
+          id="contact"
+          className="h-screen snap-always snap-center bg-black flex flex-col justify-center items-center"
+        >
+          <div className="text-4xl text-center ">CONTACT</div>
+        </section>
+      </TracingBeam>
     </div>
   );
 };
